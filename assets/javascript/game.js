3fa6615dcf8edea0ea85a0091794501e7e29780b
Shriji96 <shriji_96@yahoo.com>
2019-03-21 10:10:44 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,22 @@
-randomResult = Math.floor(Math.random() * 101) + 19;
-$('#random').html('Random-result : ')
-console.log(randomResult)
-
-
-
-
-var randomNumbers = [];
-for(var i = 0; i < 4; i++) {
-    
-    var randomNumber = Math.floor(Math.random() * 12) + 1;
-    randomNumbers.push(randomNumber)
-}
-console.log(randomNumbers);
-
 var Total = 0;
 var wins = 0;
 var losses = 0;
+randomResult = Math.floor(Math.random() * 101) + 19;
+$('#random').html('Random-result : ')
+console.log(randomResult)
+var randomNumbers = [];
+for(var i = 0; i < 4; i++) {
+    var randomNumber = Math.floor(Math.random() * 12) + 1;
+    randomNumbers.push(randomNumber)
+    console.log(randomNumbers);
+}
 
-//$('#wins').text(wins);
-//$('#losses').text(losses);
+$('#wins').text('wins: ' + wins);
+$('#losses').text('losses: ' + losses);
+$('#score').text(Total);
 
 function reset() {
-    randomResult = Math.floor(Math.random() * 120) + 19;
+    randomResult = Math.floor(Math.random() * 101) + 19;
     $('#random').html('Random-result : ')
     console.log(randomResult)
     var randomNumbers = [];
@@ -30,10 +25,9 @@ function reset() {
     var randomNumber = Math.floor(Math.random() * 12) + 1;
     randomNumbers.push(randomNumber)
 }
-Total= 0;
-$('#score').text(Total);
 }
 
+$('row-4-col-1').text()
 
 
 
